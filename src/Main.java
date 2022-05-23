4a28b2c5c25c21bbd397dbb965ddb526c73f8585
Rishat Niyazov <dxcpp@pm.me>
2022-05-23 11:26:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Enter string: ");
+        Scanner enter = new Scanner(System.in);
+        String str = enter.nextLine();
+        System.out.println(str);
+    }
+}
---
+++ src/Test/MainTest.java
@@ -0,0 +1,9 @@
+package Test;
+
+import com.sun.tools.javac.Main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest extends Main {
+
+}

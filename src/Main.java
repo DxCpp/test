8912821c54d1852f2a25dc8f66abe32cf0ff1715
Rishat Niyazov <dxcpp@pm.me>
2022-05-23 22:00:30 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.print("Enter Num 1: ");
-        Scanner enter = new Scanner(System.in);
-        float num1 = enter.nextFloat();
-
-        System.out.print("Enter Num 2: ");
-        float num2 = enter.nextFloat();
-        float sum = num1+num2;
-        System.out.println("Result: " + sum);
+            Calculator calculator = new Calculator();
     }
+}
+
+class Calculator {
+        public Calculator() {
+                System.out.print("Enter Num 1: ");
+                Scanner enter = new Scanner(System.in);
+                float num1 = enter.nextFloat();
+
+                System.out.print("Enter Num 2: ");
+                float num2 = enter.nextFloat();
+
+                System.out.println("Result: " + sum(num1,num2));
+        }
+
+        public float sum (float num1, float num2){
+                return num1 + num2;
+        }
+
 }
